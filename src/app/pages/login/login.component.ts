7ecc48c3e81f0fd7925f021c75287aa4c7caae5b
Torehpies <salupadoseiffer@gmail.com>
2024-11-30 19:55:30 +0800

    Updated login  page

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,25 +1,48 @@
-import { Component } from '@angular/core';
-import { Router } from '@angular/router';
-import { FormsModule } from '@angular/forms';
+import { Component, OnInit, inject } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { CommonModule } from '@angular/common';
-import { AuthService } from '../../auth/auth.service';
-import { AuthResponse } from '../../auth/response.model';
-
+import { AuthService } from '../../services/auth.service';
+import { Router, RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [FormsModule, CommonModule ],
+  imports: [CommonModule, ReactiveFormsModule, RouterModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css',
   providers: [AuthService],
 })
-export default class LoginComponent {
+export default class LoginComponent implements OnInit {
+  fb = inject(FormBuilder);
+  authService = inject(AuthService);
+  router = inject(Router);
+
+  loginForm !: FormGroup;
+
+  ngOnInit(): void {
+    this.loginForm = this.fb.group({
+      email: ['', Validators.compose([Validators.required, Validators.email])],
+      password: ['', Validators.required]
+    })
+  }
+
+  login(){
+    this.authService.loginService(this.loginForm.value)
+    .subscribe({
+      next:(res) => {
+      this.router.navigate(['home'])
+      },
+      error:(err) => {
+
+      }
+    })
+  }
+  /*
   email: string = '';
   password: string = '';
   errorMessage: string | null = null;
 
-  //constructor(private router: Router, /*private authService: AuthService*/) {}
+  //constructor(private router: Router, private authService: AuthService) {}
 
 
   constructor(private router: Router, private authService: AuthService) {
@@ -36,7 +59,7 @@ export default class LoginComponent {
   //   // Here you can add logic to handle login (e.g., authentication)
   //   this.router.navigate(['/dashboard']); // Replace with your target route
   // }
-  // /*onLogin() {
+  // onLogin() {
 
 
   navigateToCreateAccount() {
@@ -58,4 +81,5 @@ export default class LoginComponent {
     );
 
   }
+  */
 }
